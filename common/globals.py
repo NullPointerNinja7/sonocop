43c720b6e5871368b2c316e519c69f7489ebfcbb
NullPointerNinja7 <155996672+NullPointerNinja7@users.noreply.github.com>
2024-01-11 11:24:50 -0700

    Fix path error on linux

--- common/globals.py
+++ common/globals.py
@@ -11,7 +11,7 @@ def getmodelpath():
         # extends the sys module by a flag frozen=True and sets the app 
         # path into variable _MEIPASS'.
         application_path = sys._MEIPASS  
-        model_path = os.path.join(application_path, "common\\"  +MODEL_FILENAME)
+        model_path = os.path.join(application_path, "common", MODEL_FILENAME)
     else:
         application_path = os.path.dirname(os.path.abspath(__file__)) 
         model_path = os.path.join(application_path, MODEL_FILENAME)
